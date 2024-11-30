59c687ff937581b4a8623b4bf76c0f99c096fff4
yk-lin1021 <linalex1021@gmail.com>
2024-11-30 17:40:55 +0800

    Update 7_石門水庫.py

--- pages/7_石門水庫.py
+++ pages/7_石門水庫.py
@@ -19,14 +19,14 @@ with st.expander("See source code"):
     with st.echo():
 
         m = leafmap.Map(center=[40, -100], zoom=4)
-        cities = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv"
-        regions = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_regions.geojson"
+        cities = "https://github.com/yk-lin1021/113-1gis/raw/refs/heads/main/rain.csv"
+        regions = "https://raw.githubusercontent.com/yk-lin1021/113-1gis/refs/heads/main/1130.geojson"
 
         m.add_geojson(regions, layer_name="US Regions")
         m.add_points_from_xy(
             cities,
-            x="longitude",
-            y="latitude",
+            x="lon",
+            y="lat",
             color_column="region",
             icon_names=["gear", "map", "leaf", "globe"],
             spin=True,
