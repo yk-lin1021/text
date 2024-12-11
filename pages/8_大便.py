1b34cd06c7650bfd479380600ea3277155beb864
yk-lin1021 <linalex1021@gmail.com>
2024-12-11 15:41:13 +0800

    Update 8_大便.py

--- pages/8_大便.py
+++ pages/8_大便.py
@@ -1,1 +1,39 @@
+import streamlit as st
+import leafmap.foliumap as leafmap
+import geopandas as gpd
 
+# Load the geojson file
+data = gpd.read_file("https://raw.githubusercontent.com/yk-lin1021/113-1gis/refs/heads/main/%E5%BB%81%E6%89%80%E4%BD%8D%E7%BD%AE.geojson")
+
+# Streamlit App
+st.title("公廁互動地圖")
+
+# Sidebar options for filtering
+toilet_types = data['公廁類別'].unique()
+selected_type = st.sidebar.selectbox("選擇公廁類別", options=toilet_types, index=0)
+
+# Checkbox options for additional information
+show_accessible = st.sidebar.checkbox("顯示無障礙廁座數", value=True)
+show_parent_child = st.sidebar.checkbox("顯示親子廁座數", value=True)
+
+# Filter data based on selection
+filtered_data = data[data['公廁類別'] == selected_type]
+
+# Initialize the map
+m = leafmap.Map(center=(25.033, 121.565), zoom=12)
+
+# Add filtered data to the map
+for _, row in filtered_data.iterrows():
+    popup_info = f"<b>公廁名稱:</b> {row['公廁名稱']}<br>"
+    popup_info += f"<b>地址:</b> {row['公廁地址']}<br>"
+    popup_info += f"<b>管理單位:</b> {row['管理單位']}<br>"
+
+    if show_accessible:
+        popup_info += f"<b>無障礙廁座數:</b> {row['無障礙廁座數']}<br>"
+    if show_parent_child:
+        popup_info += f"<b>親子廁座數:</b> {row['親子廁座數']}<br>"
+
+    m.add_marker(location=(row['緯度'], row['經度']), popup=popup_info)
+
+# Display the map
+m.to_streamlit(height=700)
